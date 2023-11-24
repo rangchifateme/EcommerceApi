497e6700474bf5128aea96bb41ee625d215f9984
rangchifateme <rangchi.fateme@gmail.com>
2023-11-23 18:59:31 -0800

    #1 User Service added, DbContext Used

---
+++ Services/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceApi.Models;
+
+namespace EcommerceApi.Services
+{
+    public interface IUsersService
+    {
+        public string registerUser(UsersModel usersModel);
+    }
+}
---
+++ Services/UsersService.cs
@@ -0,0 +1,35 @@
+﻿using EcommerceApi.Entities;
+using EcommerceApi.Models;
+
+namespace EcommerceApi.Services
+{
+    public class UsersService: IUsersService
+    {
+        private readonly ApplicationDbContext _context;
+        public UsersService(ApplicationDbContext context) { 
+        _context = context;
+        }
+        public string registerUser(UsersModel usersModel) {
+            Users user = new Users
+            {
+                Id = usersModel.Id,
+                EmailAddress = usersModel.EmailAddress,
+                FirstName = usersModel.FirstName,
+                LasttName = usersModel.LastName,
+                Password = usersModel.Password,
+                PhoneNumber = usersModel.PhoneNumber
+            };
+
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "";
+        }
+    }
+}
