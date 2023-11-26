32e90834a2b2134440a2c021d77c34a72eef581f
rangchifateme <rangchi.fateme@gmail.com>
2023-11-25 17:09:34 -0800

    added anew api to get all users

--- Models/Response.cs
+++ Models/Response.cs
@@ -1,11 +1,13 @@
-﻿namespace EcommerceApi.Models
+﻿using EcommerceApi.Entities;
+
+namespace EcommerceApi.Models
 {
     public class Response
     {
         public int StatusCode { get; set; }
         public string StatusMessage { get; set; }
 
-        public List<UsersModel> ListUsers { get; set; }
+        public List<Users> ListUsers { get; set; }
         public UsersModel User { get; set; }
 
         public List<ProductsModel> ListProducrs { get; set; }
